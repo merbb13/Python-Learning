f5b77a038df24ada8250565838212ec572ab52a7
Merv Sagun <89345464+merbb13@users.noreply.github.com>
2026-03-13 01:23:49 +0800

    Delete exercise7.py

--- exercise7.py
+++
@@ -1,7 +0,0 @@
-
-print("Input password: ")
-password = input()
-if len(password) >= 8 and any(num.isdigit() for num in password):
-    print("Password is valid.")
-else:
-    print("Password is not valid.")
